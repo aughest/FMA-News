3c3ea6b7e9843ec430bbfa4b4d47bbc7e5270b8c
Aughest <aughestptr@gmail.com>
2022-01-06 13:25:48 +0700

    ads kit

--- app/src/main/java/binusmaya/binus/ac/id/fmanews/MainActivity.java
+++ app/src/main/java/binusmaya/binus/ac/id/fmanews/MainActivity.java
@@ -17,6 +17,11 @@ import java.util.List;
 import binusmaya.binus.ac.id.fmanews.Models.NewsApiResponse;
 import binusmaya.binus.ac.id.fmanews.Models.NewsHeadlines;
 
+import com.huawei.hms.ads.HwAds;
+import com.huawei.hms.ads.banner.BannerView;
+import com.huawei.hms.ads.AdParam;
+import com.huawei.hms.ads.BannerAdSize;
+
 public class MainActivity extends AppCompatActivity implements SelectListener, View.OnClickListener{
 
     RecyclerView recyclerView;
@@ -25,10 +30,26 @@ public class MainActivity extends AppCompatActivity implements SelectListener, V
     Button b1,b2,b3,b4,b5,b6,b7, btnLogin;
     SearchView searchView;
 
+    private BannerView bannerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize the HUAWEI Ads SDK.
+        HwAds.init(this);
+
+        // Obtain BannerView.
+        BannerView bannerView = findViewById(R.id.hw_banner_view);
+        // Set the ad unit ID and ad dimensions. "testw6vs28auh3" is a dedicated test ad unit ID.
+        bannerView.setAdId("testw6vs28auh3");
+        bannerView.setBannerAdSize(BannerAdSize.BANNER_SIZE_360_57);
+        // Set the refresh interval to 60 seconds.
+        bannerView.setBannerRefresh(60);
+        // Create an ad request to load an ad.
+        AdParam adParam = new AdParam.Builder().build();
+        bannerView.loadAd(adParam);
 
         searchView = findViewById(R.id.search_view);
 
